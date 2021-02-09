4b799e0a8b81bfa2b5d834a6c4b5f7125e55cca9
Angienoelhaverly <73972332+Angienoelhaverly@users.noreply.github.com>
2021-02-08 16:41:08 -0800

    creating tables

---
+++ static/js/app.js
@@ -0,0 +1,55 @@
+// code that builds the HTML table and fills it with data from data.js
+
+// import the data from data.js
+// type the following to declare a variable, tableData, using const
+const tableData = data; 
+
+// Reference the HTML table using d3
+// with this code, we decare a variable tbody & tell Javascript to look for the tbody tags in the HTML. 
+var tbody = d3.select("tbody"); 
+
+//iterate through the array of objects in our data file and then append them to a table row.
+function buildTable(data) {
+    // first clear the data
+    tbody.html("");
+    //Now that we have the start of a clean table, let's apply the forEach function.
+
+    // create another function specifically for building the table. 
+    // Data from the data.js file will be inserted into the table, row by row.
+    // This time, we'll use a forEach function, which loops through the array in the same way as a for loop. 
+    // The difference is that forEach works only with arrays. 
+    // Another benefit is that forEach can be combined with an arrow function, once again making the code more concise and easy to read.
+    data.forEach((dataRow) => {
+        // create a variable that will append a row to the table body
+        // This single line of code is doing a lot. It tells JavaScript to find the <tbody> tag within the HTML and add a table row ("tr").\
+        // <tr> tags are used for each row in a table. 
+        // Each object, or UFO sighting, in the array will be wrapped in a <tr> tag.
+        let row = tbody.append("tr");
+
+        // Next, we'll add code to loop through each field in the dataRow argument. 
+        // These fields will become table data and will be wrapped in <td> tags when they're appended to the HTML table
+        // We're already working with an array of objects, where each object is a UFO sighting. 
+        // By starting our line of code with Object.values, we're telling JavaScript to reference one object from the array of UFO sightings. 
+        // By adding (dataRow) as the argument, we are saying that we want the values to go into the dataRow. 
+        // We've added forEach((val) to specify that we want one object per row.
+        // we're telling our code put each sighting onto its own row of data. 
+        // The val argument represents each item in the object, such as the location, shape, or duration.
+        Object.values(dataRow).forEach((val) => {
+            // append each value of the object to a cell in the table.
+            let cell = row.append("td");
+            // add the values
+            cell.text(val); 
+
+            }
+        );
+
+    });
+
+}
+
+
+// With this function, we have done the following:
+// Looped through each object in the array
+// Appended a row to the HTML table
+// Added each value from the object into a cell
+
